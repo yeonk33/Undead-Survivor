954bf75a34cb663786ebcf518b8f4cc71d377451
LeeYeonkyung <55737485+yeonk33@users.noreply.github.com>
2025-01-24 02:37:04 +0900

    근접무기 레벨에 따른 배치 적용

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -23,6 +23,11 @@ public class Weapon : MonoBehaviour
 			default:
 				break;
 		}
+
+		// test
+		if (Input.GetButtonDown("Jump")) {
+			LevelUp(20, 5);
+		}
 	}
 
 	public void Init()
@@ -41,8 +46,17 @@ public class Weapon : MonoBehaviour
 	private void Batch()
 	{
 		for (int i = 0; i < Count; i++) {
-			Transform bullet = GameManager.Instance.Pool.Get(PrefabId).transform;
-			bullet.parent = transform;  // 새롭게 생성된 bullet이 Weapon 아래로 생긴다..?
+			Transform bullet;
+
+			if (i < transform.childCount) {		// 자식중에 있으면 있는거 쓰고
+				bullet = transform.GetChild(i);
+			} else {							// 없으면 오브젝트 풀링에서 가져온다
+				bullet = GameManager.Instance.Pool.Get(PrefabId).transform;
+				bullet.parent = transform;  // 새롭게 생성된 bullet이 Weapon 아래로 생긴다..?
+			}
+
+			bullet.localPosition = Vector3.zero;	// 현재 플레이어의 위치로 초기화 (Player의 자식으로 들어가있으니까)
+			bullet.localRotation = Quaternion.identity;
 
 			Vector3 rotateVector = Vector3.forward * 360 * i / Count;	// 360도를 무기 개수만큼 나눠서 배치
 			bullet.Rotate(rotateVector);
@@ -52,4 +66,15 @@ public class Weapon : MonoBehaviour
 		}
 	}
 
+	public void LevelUp(float dmg, int count)
+	{
+		this.Damage = dmg;
+		this.Count += count;
+
+		if (Id == 0) Batch();
+
+
+
+
+	}
 }
